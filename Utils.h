41da3a14380765c08451c0d270894021a5a8a2c2
Eric Planes <eric.planes@students.salle.url.edu>
2025-04-20 12:41:49 +0200

    Added more utils for the system

--- Utils.h
+++ Utils.h
@@ -5,6 +5,18 @@
 #define FALSE 0
 
 #define BYTE unsigned char
+#define STATUS unsigned char
 #define WORD unsigned short
+
+#define TIMESTAMP_SIZE sizeof("hhmmssDDMMYYYY")
+
+#define STATUS_OFF 0
+#define STATUS_LOW 1
+#define STATUS_MOD 2
+#define STATUS_HIGH 3
+#define STATUS_CRIT 4
+
+#define STATUS_ONGOING 0
+#define STATUS_DONE 1
 
 #endif
