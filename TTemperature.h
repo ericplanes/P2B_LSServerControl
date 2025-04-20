b7b84afcbd3cf409510a37235901936341e6845e
Eric Planes <eric.planes@students.salle.url.edu>
2025-04-20 13:13:58 +0200

    Updated Temperature

--- TTemperature.h
+++ TTemperature.h
@@ -3,13 +3,8 @@
 
 #include "Utils.h"
 
-// Temperature states
-#define TEMP_LOW 0
-#define TEMP_MOD 1
-#define TEMP_HIGH 2
-#define TEMP_CRIT 3
-
 // Public function
-BYTE TEMP_GetState(void);
+STATUS TEMP_GetState(void);
+WORD TEMP_GetTemperature(void);
 
 #endif
