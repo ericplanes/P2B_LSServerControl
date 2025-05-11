61d446b6ac0dc01763b9b47e4dbf5c562ea73d5a
Eric Planes <85678344+ericplanes@users.noreply.github.com>
2025-05-11 18:30:29 +0200

    Reasing pins and remove I2C and RAM (#18)

--- TI2C.h
+++
@@ -1,8 +0,0 @@
-#ifndef TI2C_H
-#define TI2C_H
-
-#include "Utils.h"
-
-BOOL I2C_ReadTimestamp(BYTE *hhmmssDDMMYYYY);
-
-#endif
--- TRam.h
+++
@@ -1,9 +0,0 @@
-#ifndef TRAM_H
-#define TRAM_H
-
-#include "Utils.h"
-
-// Stores a temperature value into RAM progressively (simulated)
-BOOL RAM_StoreTemperature(WORD temperature);
-
-#endif
